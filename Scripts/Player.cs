8b6d6eee2cb04088ff57a0f593dfece1c9c3c6a3
MzaxnaV <manav.zxa@gmail.com>
2023-05-13 18:45:04 +0100

    fix ugly tiles

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,11 +2,14 @@ using Godot;
 using System;
 using System.Diagnostics;
 
-public partial class Player : CharacterBody2D
+public partial class Player : AnimatedSprite2D
 {
 	[Export] private NodePath _tileMapPath;
+	[Export] private Vector2I _tilePos;
 	
 	private TileMap _tileMap;
+
+	private Vector2I _size = new Vector2I(36, 36);
 
 	public override void _Ready()
 	{
@@ -19,11 +22,18 @@ public partial class Player : CharacterBody2D
 			Debug.Fail("TileMapPath path not set, check inspector.");
 		}
 
-		GetChild<AnimatedSprite2D>(0).Play();
+		Play();
 
-		var data = _tileMap.GetCellTileData(1, Vector2I.Zero);
-		
-		Debug.Print(data.GetCustomData("id").ToString());
+		if (_tileMap.GetCellTileData(1, _tilePos).GetCustomData("id").As<int>() != 6)
+		{
+			Debug.Fail("Start Position not valid, check inspector and set tile pos.");
+		}
+		else
+		{
+			_tileMap.SetCell(1, _tilePos);
+			Debug.Print(_tilePos.ToString());
+			Position = _size * _tilePos;
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
