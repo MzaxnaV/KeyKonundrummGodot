fd3240c686c0d0642e5d8b56175b827b092ab91e
MzaxnaV <manav.zxa@gmail.com>
2023-05-13 19:43:46 +0100

    add turn base

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,14 +5,16 @@ using System.Diagnostics;
 public partial class Player : AnimatedSprite2D
 {
 	[Export] private NodePath _tileMapPath;
+	[Export] private NodePath _barPath;
 	[Export] private Vector2I _tilePos;
 	
 	[Signal] public delegate bool UiEventHandler();
 	
-	
 	private TileMap _tileMap;
-
-	private Vector2I _size = new Vector2I(36, 36);
+	private Sprite2D _bar;
+	private bool _isMoving;
+	private Timer _timer;
+	private readonly Vector2I _size = new (36, 36);
 
 	public override void _Ready()
 	{
@@ -24,6 +26,18 @@ public partial class Player : AnimatedSprite2D
 		{
 			Debug.Fail("TileMapPath path not set, check inspector.");
 		}
+		
+		if (_barPath != null && _barPath.ToString() != "")
+		{
+			_bar = GetNode<Sprite2D>(_barPath);
+		}
+		else
+		{
+			Debug.Fail("BarSprite path not set, check inspector.");
+		}
+
+		_timer = GetChild<Timer>(0);
+		_timer.Timeout += OnTimerTimeout;
 
 		Play();
 
@@ -41,6 +55,43 @@ public partial class Player : AnimatedSprite2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		
+		if (_isMoving)
+		{
+			return;
+		}
+
+		var direction = new Vector2I(0, 0);
+
+		if (Input.IsActionPressed("ui_up"))
+		{
+			direction.Y -= 1;
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			direction.Y += 1;
+		}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			direction.X -= 1;
+		}
+		if (Input.IsActionPressed("ui_right"))
+		{
+			direction.X += 1;
+		}
+
+		if (direction != new Vector2I(0, 0))
+		{
+			_tilePos += direction;
+			Position = _size * _tilePos;
+			_isMoving = true;
+			
+			_timer.Start();
+			// EmitSignal(nameof(UiEventHandler));
+		}
+	}
+
+	private void OnTimerTimeout()
+	{
+		_isMoving = false;
 	}
 }
