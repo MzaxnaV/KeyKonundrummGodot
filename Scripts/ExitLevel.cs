f97732209a34a75190fb6c71bb2dbd16181bc3d7
Manav Chawra (Student) <you@example.com>
2023-05-14 20:25:57 +0100

    finished key pick/drop

--- Scripts/ExitLevel.cs
+++ Scripts/ExitLevel.cs
@@ -1,5 +1,6 @@
 using Godot;
-using System;
+
+namespace GodotGameJamGame.Scripts;
 
 public partial class ExitLevel : Node
 {
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,28 +1,177 @@
-using Godot;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using Godot;
 
-public enum key
+namespace GodotGameJamGame.Scripts;
+
+public enum DirectionKey
 {
-	Up, Down, Left, Right
+	None = -1, Up = 1, Down, Left, Right
+}
+
+public enum TileValue
+{
+	Empty = 0, Wall, Move, Drop, PickUp, Portal, Start, End
 }
 
 public partial class Player : AnimatedSprite2D
 {
+	private static readonly string _className = "Player";
+	
 	[Export] private NodePath _tileMapPath;
 	[Export] private NodePath _barPath;
+	[Export] private NodePath _gamePath;
 	[Export] private Vector2I _tilePos;
-	[Signal] public delegate bool UiEventHandler();
+
+	[Export] private NodePath _upRedPath;
+	[Export] private NodePath _upBluePath;
 	
+	[Export] private NodePath _downRedPath;
+	[Export] private NodePath _downBluePath;
+	
+	[Export] private NodePath _leftRedPath;
+	[Export] private NodePath _leftBluePath;
+    
+	[Export] private NodePath _rightRedPath;
+	[Export] private NodePath _rightBluePath;
+
+	[Export] private Texture2D _arrowTex;
+
+	private Sprite2D _upRed;
+	private Sprite2D _upBlue;
+    
+	private Sprite2D _downRed;
+	private Sprite2D _downBlue;
+	
+	private Sprite2D _leftRed;
+	private Sprite2D _leftBlue;
+    
+	private Sprite2D _rightRed;
+	private Sprite2D _rightBlue;
+
+	private HashSet<DirectionKey> _enabledKeys = new();
+
 	private TileMap _tileMap;
 	private Sprite2D _bar;
 	private bool _isMoving;
 	private Timer _timer;
-	private bool _keyUp;
-	private bool _keyDown;
-	private bool _keyLeft;
-	private bool _keyRight;
+
+	private static readonly Vector2I Size = new (36, 36);
+	private static readonly Vector2I HalfSize = new (18, 18);
+
+	private const string IdLayer = "id";
+	private const string KeyLayer = "key";
+
+	private Godot.Collections.Dictionary<Vector2I, Sprite2D> arrows = new ();
+
+	private void SetPath(NodePath path, ref Sprite2D sprite)
+	{
+		if (path != null && path.ToString() != "")
+		{
+			sprite = GetNode<Sprite2D>(path);
+		}
+		else
+		{
+			Debug.Fail(_className + ": path not set, check inspector.");
+		}
+	}
+
+	private void RemoveKey(DirectionKey key)
+	{
+		_enabledKeys.Remove(key);
+		switch (key)
+		{
+			case DirectionKey.Up:
+				_upBlue.Visible = false;
+				_upRed.Visible = true;
+				break;
+
+			case DirectionKey.Down:
+				_downBlue.Visible = false;
+				_downRed.Visible = true;
+				break;
+
+			case DirectionKey.Left:
+				_leftBlue.Visible = false;
+				_leftRed.Visible = true;
+				break;
+
+			case DirectionKey.Right:
+				_rightBlue.Visible = false;
+				_rightRed.Visible = true;
+				break;
+
+			default:
+				GD.Print("Invalid direction key");
+				break;
+		}
+	}
 	
-	private readonly Vector2I _size = new (36, 36);
+	private void AddKey(DirectionKey key)
+	{
+		_enabledKeys.Add(key);
+		switch (key)
+		{
+			case DirectionKey.Up:
+				_upBlue.Visible = true;
+				_upRed.Visible = false;
+				break;
+
+			case DirectionKey.Down:
+				_downBlue.Visible = true;
+				_downRed.Visible = false;
+				break;
+
+			case DirectionKey.Left:
+				_leftBlue.Visible = true;
+				_leftRed.Visible = false;
+				break;
+
+			case DirectionKey.Right:
+				_rightBlue.Visible = true;
+				_rightRed.Visible = false;
+				break;
+
+			default:
+				GD.Print("Invalid direction key");
+				break;
+		}
+	}
+
+	private void AddArrow(Vector2I tilePos, DirectionKey key)
+	{
+		Sprite2D arrow = new Sprite2D();
+		arrow.Texture = _arrowTex;
+		arrow.Rotation = key switch
+		{
+			DirectionKey.Down => Mathf.Pi,
+			DirectionKey.Left => -Mathf.Pi / 2,
+			DirectionKey.Right => Mathf.Pi / 2,
+			_ => arrow.Rotation
+		};
+		
+		GetParent().AddChild(arrow);
+
+		arrow.Position = Position + HalfSize;
+		
+		arrows.Add(tilePos, arrow);
+	}
+	
+	private void RemoveArrow(Vector2I tilePos)
+	{
+		if (arrows.ContainsKey(tilePos))
+		{
+			var arrow = arrows[tilePos];
+			arrows.Remove(tilePos);
+			arrow.QueueFree();
+		}
+		else
+		{
+			Debug.Fail("No arrow to remove, check RemoveArrow()");
+		}
+	}
 
 	public override void _Ready()
 	{
@@ -49,21 +198,34 @@ public partial class Player : AnimatedSprite2D
 
 		Play();
 
-		if (GetCellData(_tilePos.X, _tilePos.Y) != 6)
+		if (GetCellData(_tilePos.X, _tilePos.Y, IdLayer) != (int)TileValue.Start)
 		{
 			Debug.Fail("Start Position not valid, check inspector and set tile pos.");
 		}
 		else
 		{
 			_tileMap.SetCell(1, _tilePos);
-			Debug.Print(_tilePos.ToString());
-			Position = _size * _tilePos;
+			Position = Size * _tilePos;
 		}
 		
-		_keyUp = true;
-		_keyDown = true;
-		_keyLeft = true;
-		_keyRight = true;
+		_enabledKeys.Add(DirectionKey.Up);
+		_enabledKeys.Add(DirectionKey.Down);
+		_enabledKeys.Add(DirectionKey.Right);
+		_enabledKeys.Add(DirectionKey.Left);
+		
+		SetPath(_leftRedPath, ref _leftRed);
+		SetPath(_leftBluePath, ref _leftBlue);
+
+		SetPath(_rightRedPath, ref _rightRed);
+		SetPath(_rightBluePath, ref _rightBlue);
+
+		SetPath(_upRedPath, ref _upRed);
+		SetPath(_upBluePath, ref _upBlue);
+        
+		SetPath(_downRedPath, ref _downRed);
+		SetPath(_downBluePath, ref _downBlue);
+		
+		
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -75,32 +237,29 @@ public partial class Player : AnimatedSprite2D
 		
 		var direction = new Vector2I(0, 0);
 		
-		if (_keyUp && Input.IsActionJustPressed("ui_up"))
+		if (_enabledKeys.Contains(DirectionKey.Up) && Input.IsActionJustPressed("ui_up"))
 		{
-			SetTurn(ref direction, ref _keyUp, 0, -1);
+			SetTurn(ref direction, DirectionKey.Up, 0, -1);
 		}
-		if (_keyDown && Input.IsActionJustPressed("ui_down"))
+		if (_enabledKeys.Contains(DirectionKey.Down) && Input.IsActionJustPressed("ui_down"))
 		{
-			SetTurn(ref direction, ref _keyDown, 0, 1);
+			SetTurn(ref direction, DirectionKey.Down, 0, 1);
 		}
-		if (_keyLeft && Input.IsActionJustPressed("ui_left"))
+		if (_enabledKeys.Contains(DirectionKey.Left) && Input.IsActionJustPressed("ui_left"))
 		{
-			SetTurn(ref direction, ref _keyLeft, -1, 0);
+			SetTurn(ref direction, DirectionKey.Left, -1, 0);
 		}
-		if (_keyRight && Input.IsActionJustPressed("ui_right"))
+		if (_enabledKeys.Contains(DirectionKey.Right) && Input.IsActionJustPressed("ui_right"))
 		{
-			SetTurn(ref direction, ref _keyRight, 1, 0);
+			SetTurn(ref direction, DirectionKey.Right, 1, 0);
 		}
 
-		if (direction != new Vector2I(0, 0))
-		{
-			_tilePos += direction;
-			Position = _size * _tilePos;
-			_isMoving = true;
+		if (direction == new Vector2I(0, 0)) return;
+		_tilePos += direction;
+		Position = Size * _tilePos;
+		_isMoving = true;
 			
-			_timer.Start();
-			// EmitSignal(nameof(UiEventHandler));
-		}
+		_timer.Start();
 	}
 
 	private void OnTimerTimeout()
@@ -108,45 +267,60 @@ public partial class Player : AnimatedSprite2D
 		_isMoving = false;
 	}
 	
-	private int GetCellData(int posX, int posY)
+	private int GetCellData(int posX, int posY, string layerName)
 	{
+		var nullVal = (layerName == "id") ? 0 : -1;
 		var tileData = _tileMap.GetCellTileData(1, new Vector2I(posX, posY));
-		return tileData != null ? tileData.GetCustomData("id").As<int>() : 0;
+		return tileData != null ? tileData.GetCustomData(layerName).As<int>() : nullVal;
 	}
 
-	private void SetTurn(ref Vector2I direction, ref bool key, int x, int y)
+	private void PrintCellTileData(Vector2I tilePos, string stuff)
 	{
-		switch (GetCellData(_tilePos.X, _tilePos.Y))
+		GD.Print(stuff + ", Cell" + tilePos + " : " + Enum.GetName((TileValue)GetCellData(_tilePos.X, _tilePos.Y, IdLayer)));
+	}
+	
+	private void PrintCellKeyData(Vector2I tilePos, string stuff)
+	{
+		GD.Print(stuff + ", Cell" + tilePos + " : " + Enum.GetName((DirectionKey)GetCellData(_tilePos.X, _tilePos.Y, KeyLayer)));
+	}
+
+	private void SetTurn(ref Vector2I direction, DirectionKey key, int x, int y)
+	{
+		switch ((TileValue)GetCellData(_tilePos.X, _tilePos.Y, IdLayer))
 		{
-			case 3:
-				Debug.Print("Drop");
-				key = false;
+			case TileValue.Drop:
+				RemoveKey(key);
 				
-				GD.Print("Tile value before: " + GetCellData(_tilePos.X, _tilePos.Y));
 				_tileMap.SetCell(1, _tilePos, 7, new Vector2I(1, 0), 5);
-				GD.Print("Tile value after: " + GetCellData(_tilePos.X, _tilePos.Y));
-				
+				_tileMap.GetCellTileData(1, _tilePos).SetCustomData(KeyLayer, (int)key);
+
+				AddArrow(_tilePos, key);
+
 				break;
-			case 4:
-				Debug.Print("Pick");
-				// change the key to Drop
-				key = true;
+			case TileValue.PickUp:
+				AddKey((DirectionKey)GetCellData(_tilePos.X, _tilePos.Y, KeyLayer));
+				
+				_tileMap.SetCell(1, _tilePos, 7, new Vector2I(1, 0), 3);
+				
+				RemoveArrow(_tilePos);
+
 				break;
 		}
 
 		// check the cell data
-		switch (GetCellData(_tilePos.X + x, _tilePos.Y + y))
+		switch ((TileValue)GetCellData(_tilePos.X + x, _tilePos.Y + y, IdLayer))
 		{
-			case 1:
+			case TileValue.Wall:
 				break;
-			case 2:
-				switch (GetCellData(_tilePos.X + 2 * x, _tilePos.Y + 2 * y))
+			case TileValue.Move:
+				switch ((TileValue)GetCellData(_tilePos.X + 2 * x, _tilePos.Y + 2 * y, IdLayer))
 				{
-					case 1:
+					case TileValue.Wall:
 						direction.Y += y;
 						direction.X += x;
 						break;
 					/*
+					 TODO: think this through
 					case 3:
 						direction.Y += y;
 						direction.X += x;
